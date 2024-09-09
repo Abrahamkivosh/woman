cda104402c61bb8427620315baced3ea708f6450
stanleymwinde <salvadorethorbide8@gmail.com>
2024-09-09 11:42:29 +0300

    contact us page

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,19 @@
+import { ContactDetails } from "@/components/contact";
+import { Heading, Stack } from "@chakra-ui/react";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Stack spacing={6} pt={10}>
+        <Heading as="h1" size={"3xl"} textAlign={"center"}>
+          Contact Us Below
+        </Heading>
+      </Stack>
+
+      <ContactDetails />
+    </>
+  );
+};
+
+export default page;
