402efa87ef7edd658375147191228b1b7264c5f4
Abrahamkivosh <abrahamkivosh@gmail.com>
2024-09-10 17:20:11 +0300

    removed bugs

--- components/who-we-are/index.ts
+++ components/who-we-are/index.ts
@@ -1,5 +1,3 @@
-export { default as OurHistoryContent } from "./our-history-content";
-export { default as OurImpactContent } from "./our-impact";
 export { default as PartnersDetailContent } from "./partners-detail-content";
 export { default as PartnersListing } from "./partners-listing";
 export { default as OurTeam } from "./team";
